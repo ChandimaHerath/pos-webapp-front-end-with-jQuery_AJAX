cec6524e7347addfd5d13250f153bb6ffe16f677
ChandimaHerath <chandimaherath@hotmail.com>
2021-10-21 10:42:54 +0530

    customer order item search order dto added

---
+++ ts/dto/item.ts
@@ -0,0 +1,7 @@
+export class Items{
+    public constructor(
+        public code:string, 
+        public description:string, 
+        public unitPrice:number, 
+        public qtyOnHand:number){}
+}
